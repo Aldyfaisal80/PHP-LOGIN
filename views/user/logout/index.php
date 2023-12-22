0b492dab0b23a6fa073a793c47e694af975eefca
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2023-12-22 17:25:43 +0700

    first commit

---
+++ views/user/logout/index.php
@@ -0,0 +1,11 @@
+<?
+session_start();
+
+if(!isset($_SESSION["login"])) {
+    header("Location: 'http://localhost/cobaphp2/views/user/login/");
+}
+
+session_destroy();
+$_SESSION = [];
+session_unset();
+header("Location: http://localhost/cobaphp2");
